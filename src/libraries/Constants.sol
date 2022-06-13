e75d91dba6d37ea0de6c48724bd583b8eb34b67a
zhimao <zhimao_liu@icloud.com>
2022-06-13 16:20:33 -0700

    ACL

--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -1,11 +1,14 @@
 pragma solidity 0.8.14;
 
 library Constants {
-    uint8 internal constant MINTER_ROLE = 0;
-    uint8 internal constant ENGINE_GOV_ROLE = 1;
-    // deconstruct the types in the struct
+
+    // Access Control for ProfileNFT
+    uint8 internal constant PROFILE_MINTER_ROLE = 0;
     bytes4 internal constant CREATE_PROFILE_ID =
-        bytes4(keccak256(bytes("createProfile(address,(address,string,string))")));
+        bytes4(keccak256(bytes("createProfile(address,(string,string))")));
+
+    // Access Control for CyebreEngine
+    uint8 internal constant ENGINE_GOV_ROLE = 1;
     bytes4 internal constant SET_SIGNER =
         bytes4(keccak256(bytes("setSigner(address)")));
     bytes4 internal constant SET_PROFILE_ADDR =
