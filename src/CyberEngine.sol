dfc206834288d3f7932e3c8bb5f379dd02365d20
zhimao <zhimao_liu@icloud.com>
2022-06-13 16:18:19 -0700

    merge

---
+++ src/CyberEngine.sol
@@ -0,0 +1,56 @@
+pragma solidity 0.8.14;
+
+import "solmate/auth/authorities/RolesAuthority.sol";
+import {Authority} from "solmate/auth/Auth.sol";
+
+contract CyberEngine is Auth {
+
+  address public profileAddress;
+  address public boxAddress;
+  address public signer;
+
+  constructor(
+      address _owner,
+      address _profileAddress,
+      address _boxAddress,
+      RolesAuthority _rolesAuthority
+  ) Auth(_owner, _rolesAuthority) {
+      signer = _owner;
+      profileAddress = _profileAddress;
+      boxAddress = _boxAddress;
+  }
+
+  function setSigner(address _signer) 
+      external
+      requiresAuth
+  {
+      signer = _signer;
+  }
+
+  function setProfileAddress(address _profileAddress) 
+      external
+      requiresAuth
+  {
+      profileAddress = _profileAddress;
+  }
+
+  function setBoxAddress(address _boxAddress) 
+      external
+      requiresAuth
+  {
+      boxAddress = _boxAddress;
+  }
+
+  function getSigner() public view returns (address) {
+      return signer;
+  }
+
+  function getProfileAddress() public view returns (address) {
+      return profileAddress;
+  }
+
+  function getBoxAddress() public view returns (address) {
+      return boxAddress;
+  }
+}
+
--- src/libraries/Constants.sol
+++ src/libraries/Constants.sol
@@ -1,10 +1,17 @@
 pragma solidity 0.8.14;
 
 library Constants {
-    // Access Control for ProfileNFT
-    uint8 internal constant PROFILE_MINTER_ROLE = 0;
+    uint8 internal constant MINTER_ROLE = 0;
+    uint8 internal constant ENGINE_GOV_ROLE = 1;
+    // deconstruct the types in the struct
     bytes4 internal constant CREATE_PROFILE_ID =
-        bytes4(keccak256(bytes("createProfile(address,(string,string))")));
+        bytes4(keccak256(bytes("createProfile(address,(address,string,string))")));
+    bytes4 internal constant SET_SIGNER =
+        bytes4(keccak256(bytes("setSigner(address)")));
+    bytes4 internal constant SET_PROFILE_ADDR =
+        bytes4(keccak256(bytes("setProfileAddress(address)")));
+    bytes4 internal constant SET_BOX_ADDR =
+        bytes4(keccak256(bytes("setBoxAddress(address)")));
 
     // Parameters
     uint8 internal constant MAX_HANDLE_LENGTH = 27;
