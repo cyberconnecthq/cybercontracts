86753fbb08cfcef465ef8319278a6d4db93486d1
Ryan Li <ryan@ryanli.me>
2022-06-15 13:43:35 -0700

    fix: remove unused contract; add more commments to Initializable

--- src/upgradeability/Initializable.sol
+++ src/upgradeability/Initializable.sol
@@ -2,8 +2,29 @@
 
 pragma solidity 0.8.14;
 
-// Inspired by Openzeppelin's Initializable contract, but simplified for our use case.
-// Explicitly removed support for modifier initializer on constructor.
+/**
+ * Inspired by Openzeppelin's Initializable contract, but simplified for our use case.
+ * Explicitly removed support for modifier `initializer` on constructor.
+ * Only use `initializer` modifier on the outermost contract and use `onlyInitializing` on the
+ * dependencies's init functions.
+ *
+ * For example:
+ *
+ * [.hljs-theme-light.nopadding]
+ * ```
+ * contract Parent, Initializable {
+ *     uint256 key;
+ *     function __Parent_Init(uint256 _key) onlyInitializing public {
+ *         key = _key;
+ *     }
+ * }
+ * contract Child is Parent, Initializable {
+ *     function initialize(uint256 _key) initializer external {
+ *         __Parent_Init(_key);
+ *     }
+ * }
+ * ```
+ */
 abstract contract Initializable {
     uint8 private _initialized;
     bool private _initializing;
@@ -15,10 +36,12 @@ abstract contract Initializable {
             "Contract already initialized"
         );
         _initialized = 1;
+        // TODO: this is dead code after we removed modifier initializer on constructor, kept for now
         if (isTopLevelCall) {
             _initializing = true;
         }
         _;
+        // TODO: this is dead code after we removed modifier initializer on constructor, kept for now
         if (isTopLevelCall) {
             _initializing = false;
         }
