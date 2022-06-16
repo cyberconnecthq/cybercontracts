fe9e5d4de1978ee7c35f3b3b4186a1ca6f3acc1a
SamixDev <samer.hd.ah@gmail.com>
2022-06-16 15:17:06 +0300

    fix: deploy script

--- script/ProfileNFT.s.sol
+++ script/ProfileNFT.s.sol
@@ -5,24 +5,17 @@ import "forge-std/Script.sol";
 import "../src/ProfileNFT.sol";
 import "../src/libraries/Constants.sol";
 import "../src/libraries/DataTypes.sol";
-import "solmate/auth/authorities/RolesAuthority.sol";
+import "../src/base/RolesAuthority.sol";
 
 contract DeployScript is Script {
     function run() external {
         vm.startBroadcast();
 
-        ProfileNFT token;
         RolesAuthority rolesAuthority;
 
         rolesAuthority = new RolesAuthority(
             address(this),
             Authority(address(0))
-        );
-        token = new ProfileNFT(
-            "TestProfile",
-            "TP",
-            address(this),
-            rolesAuthority
         );
 
         vm.stopBroadcast();
