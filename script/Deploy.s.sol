b8b50de3f3242f5b406010b32a072c140d9edc90
Ryan Li <ryan@ryanli.me>
2022-06-23 15:46:49 -0700

    fix: pre-calc engine proxy adderss

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -70,27 +70,26 @@ contract DeployScript is Script {
     }
 
     function run() external {
-        console.log(msg.sender);
-        address deployer = msg.sender;
         // XXX: input deployer's nonce here. get it by `cast nonce`
         uint256 nonce = 0;
         vm.startBroadcast();
 
         // TODO: emergency admin
-        address emergencyAdmin = address(0x1890);
+        // address emergencyAdmin = address(0x1890);
 
         // 1. RolesAuthority
         RolesAuthority rolesAuthority = new RolesAuthority(
             address(this),
             Authority(address(0))
         );
-        _requiresContractAddress(deployer, nonce, address(rolesAuthority));
-        // 2. Deploy CyberEngine, pass this address to subscribebeacon and profile
-        CyberEngine engine = new CyberEngine();
-        _requiresContractAddress(deployer, nonce + 1, address(engine));
+        _requiresContractAddress(msg.sender, nonce, address(rolesAuthority));
+        // 2. Calc CyberEngine address
+        CyberEngine engineImpl = new CyberEngine();
+        address engineAddr = _calcContractAddress(msg.sender, nonce + 8);
+        _requiresContractAddress(msg.sender, nonce + 1, address(engineImpl));
         // 3. Deploy ProfileNFT Impl
-        ProfileNFT profileImpl = new ProfileNFT(address(engine));
-        _requiresContractAddress(deployer, nonce + 2, address(profileImpl));
+        ProfileNFT profileImpl = new ProfileNFT(address(engineAddr));
+        _requiresContractAddress(msg.sender, nonce + 2, address(profileImpl));
         // 4. Deploy Proxy for ProfileNFT
         bytes memory initData = abi.encodeWithSelector(
             ProfileNFT.initialize.selector,
@@ -102,43 +101,49 @@ contract DeployScript is Script {
             address(profileImpl),
             initData
         );
-        _requiresContractAddress(deployer, nonce + 3, address(profileProxy));
-        // address profileAddr = address(profileProxy);
-        // 5. Deploy BoxNFT Impl
-        BoxNFT boxImpl = new BoxNFT(address(engine));
-        _requiresContractAddress(deployer, nonce + 4, address(boxImpl));
-        // 6. Deploy Proxy for BoxNFT
-        bytes memory boxInitData = abi.encodeWithSelector(
-            BoxNFT.initialize.selector,
-            "CyberBox",
-            "CYBER_BOX"
-        );
-        ERC1967Proxy boxProxy = new ERC1967Proxy(address(boxImpl), boxInitData);
-        _requiresContractAddress(deployer, nonce + 5, address(boxProxy));
-        // address boxAddr = address(boxProxy);
+        _requiresContractAddress(msg.sender, nonce + 3, address(profileProxy));
+        ERC1967Proxy boxProxy;
+        {
+            // scope to avoid stack too deep  errors
+            // 5. Deploy BoxNFT Impl
+            BoxNFT boxImpl = new BoxNFT(address(engineAddr));
+            _requiresContractAddress(msg.sender, nonce + 4, address(boxImpl));
+            // 6. Deploy Proxy for BoxNFT
+            bytes memory boxInitData = abi.encodeWithSelector(
+                BoxNFT.initialize.selector,
+                "CyberBox",
+                "CYBER_BOX"
+            );
+            boxProxy = new ERC1967Proxy(address(boxImpl), boxInitData);
+            _requiresContractAddress(msg.sender, nonce + 5, address(boxProxy));
+        }
         // 7. Deploy SubscribeNFT Impl
         SubscribeNFT subscribeImpl = new SubscribeNFT(
-            address(engine),
+            address(engineAddr),
             address(profileProxy)
         );
-        _requiresContractAddress(deployer, nonce + 6, address(subscribeImpl));
+        _requiresContractAddress(msg.sender, nonce + 6, address(subscribeImpl));
         // 8. Deploy Subscribe Beacon
         UpgradeableBeacon subscribeBeacon = new UpgradeableBeacon(
             address(subscribeImpl),
-            address(engine)
+            address(engineAddr)
         );
-        _requiresContractAddress(deployer, nonce + 7, address(subscribeBeacon));
+        _requiresContractAddress(
+            msg.sender,
+            nonce + 7,
+            address(subscribeBeacon)
+        );
         // 9. Deploy Proxy for CyberEngine
         bytes memory data = abi.encodeWithSelector(
             CyberEngine.initialize.selector,
-            emergencyAdmin,
+            msg.sender, // TODO: emergency admin
             address(profileProxy),
             address(boxProxy),
             address(subscribeBeacon),
             address(rolesAuthority)
         );
-        ERC1967Proxy engineProxy = new ERC1967Proxy(address(engine), data);
-        _requiresContractAddress(deployer, nonce + 8, address(engineProxy));
+        ERC1967Proxy engineProxy = new ERC1967Proxy(address(engineImpl), data);
+        _requiresContractAddress(msg.sender, nonce + 8, address(engineProxy));
 
         // TODO: set correct role capacity
         // TODO: do a health check. verify everything
