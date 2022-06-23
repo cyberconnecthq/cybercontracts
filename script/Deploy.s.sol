f2ee5016fe47ef25270cfb59764c42e8697ca7b8
Ryan Li <ryan@ryanli.me>
2022-06-23 15:32:29 -0700

    wip: verify deployed contract address

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "forge-std/console.sol";
 import { ProfileNFT } from "../src/ProfileNFT.sol";
 import { RolesAuthority } from "../src/base/RolesAuthority.sol";
 import { CyberEngine } from "../src/CyberEngine.sol";
@@ -11,8 +12,68 @@ import { Authority } from "../src/base/Auth.sol";
 import { UpgradeableBeacon } from "../src/upgradeability/UpgradeableBeacon.sol";
 import { ERC1967Proxy } from "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
+//     return address(keccak256(0xd6, 0x94, this, 0x80));
+// return address(keccak256(0xd6, 0x94, this, nonce));
+
 contract DeployScript is Script {
+    function _calcContractAddress(address _owner, uint256 _nonce)
+        internal
+        view
+        returns (address)
+    {
+        if (_nonce == 0) {
+            return
+                address(
+                    uint160(
+                        uint256(
+                            keccak256(
+                                abi.encodePacked(
+                                    bytes1(0xd6),
+                                    bytes1(0x94),
+                                    _owner,
+                                    bytes1(0x80)
+                                )
+                            )
+                        )
+                    )
+                );
+        }
+        return
+            address(
+                uint160(
+                    uint256(
+                        keccak256(
+                            abi.encodePacked(
+                                bytes1(0xd6),
+                                bytes1(0x94),
+                                _owner,
+                                bytes1(uint8(_nonce))
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function _requiresContractAddress(
+        address deployer,
+        uint256 nonce,
+        address c
+    ) internal {
+        address calc = _calcContractAddress(deployer, nonce);
+        if (c != calc) {
+            console.log("nonce ", nonce);
+            console.log("calc ", calc);
+            console.log("got ", c);
+            revert("contract address mismatch");
+        }
+    }
+
     function run() external {
+        console.log(msg.sender);
+        address deployer = msg.sender;
+        // XXX: input deployer's nonce here. get it by `cast nonce`
+        uint256 nonce = 0;
         vm.startBroadcast();
 
         // TODO: emergency admin
@@ -23,11 +84,13 @@ contract DeployScript is Script {
             address(this),
             Authority(address(0))
         );
+        _requiresContractAddress(deployer, nonce, address(rolesAuthority));
         // 2. Deploy CyberEngine, pass this address to subscribebeacon and profile
         CyberEngine engine = new CyberEngine();
-        // address engineAddr = address(engine);
+        _requiresContractAddress(deployer, nonce + 1, address(engine));
         // 3. Deploy ProfileNFT Impl
         ProfileNFT profileImpl = new ProfileNFT(address(engine));
+        _requiresContractAddress(deployer, nonce + 2, address(profileImpl));
         // 4. Deploy Proxy for ProfileNFT
         bytes memory initData = abi.encodeWithSelector(
             ProfileNFT.initialize.selector,
@@ -39,9 +102,11 @@ contract DeployScript is Script {
             address(profileImpl),
             initData
         );
+        _requiresContractAddress(deployer, nonce + 3, address(profileProxy));
         // address profileAddr = address(profileProxy);
         // 5. Deploy BoxNFT Impl
         BoxNFT boxImpl = new BoxNFT(address(engine));
+        _requiresContractAddress(deployer, nonce + 4, address(boxImpl));
         // 6. Deploy Proxy for BoxNFT
         bytes memory boxInitData = abi.encodeWithSelector(
             BoxNFT.initialize.selector,
@@ -49,17 +114,20 @@ contract DeployScript is Script {
             "CYBER_BOX"
         );
         ERC1967Proxy boxProxy = new ERC1967Proxy(address(boxImpl), boxInitData);
+        _requiresContractAddress(deployer, nonce + 5, address(boxProxy));
         // address boxAddr = address(boxProxy);
         // 7. Deploy SubscribeNFT Impl
         SubscribeNFT subscribeImpl = new SubscribeNFT(
             address(engine),
             address(profileProxy)
         );
+        _requiresContractAddress(deployer, nonce + 6, address(subscribeImpl));
         // 8. Deploy Subscribe Beacon
         UpgradeableBeacon subscribeBeacon = new UpgradeableBeacon(
             address(subscribeImpl),
             address(engine)
         );
+        _requiresContractAddress(deployer, nonce + 7, address(subscribeBeacon));
         // 9. Deploy Proxy for CyberEngine
         bytes memory data = abi.encodeWithSelector(
             CyberEngine.initialize.selector,
@@ -70,6 +138,7 @@ contract DeployScript is Script {
             address(rolesAuthority)
         );
         ERC1967Proxy engineProxy = new ERC1967Proxy(address(engine), data);
+        _requiresContractAddress(deployer, nonce + 8, address(engineProxy));
 
         // TODO: set correct role capacity
         // TODO: do a health check. verify everything
