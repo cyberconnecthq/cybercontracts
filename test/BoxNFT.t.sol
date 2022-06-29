f1b806f267f9dbe4a6df21642ca6fd247b6e3cea
Songqi(Steven) Pu <45271050+pusongqi@users.noreply.github.com>
2022-06-29 14:30:52 -0700

    feat: pausable test (#112)
    Finished paused function. Waiting for review

--- test/BoxNFT.t.sol
+++ test/BoxNFT.t.sol
@@ -47,4 +47,43 @@ contract BoxNFTTest is Test {
         token.mint(alice);
         assertEq(token.balanceOf(alice), 1);
     }
+
+    // set prank as non engine, then try to pause, should be reverted
+    function testCannotPauseFromNonEngine() public {
+        vm.expectRevert("Only Engine");
+        vm.prank(address(0));
+        token.pause(true);
+    }
+
+    // set prank as engine, then try to pause again, since it was paused already(from initialization), it can't pause again
+    function testCannotPauseWhenAlreadyPaused() public {
+        vm.prank(engine);
+        vm.expectRevert("Pausable: paused");
+        token.pause(true);
+    }
+
+    // we first unpause, verify, then we unpause, then verify, we can't unpause when already unpaused
+    function testCannotUnpauseWhenAlreadyUnpaused() public {
+        vm.startPrank(engine);
+        token.pause(false);
+        vm.expectRevert("Pausable: not paused");
+        token.pause(false);
+    }
+
+    // we first unpause, verify, then we unpause, then verify
+    function testPause() public {
+        vm.startPrank(engine);
+        token.pause(false);
+        assertEq(token.paused(), false);
+        token.pause(true);
+        assertEq(token.paused(), true);
+    }
+
+    // we first verify that the contracy is paused, then unpause, and verify
+    function testUnpause() public {
+        vm.startPrank(engine);
+        assertEq(token.paused(), true);
+        token.pause(false);
+        assertEq(token.paused(), false);
+    }
 }
