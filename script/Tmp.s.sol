d6f8acdb357e194ff6be6fbc1636ea7a0f954236
Ryan Li <ryan@ryanli.me>
2022-07-17 21:47:23 -0700

    release: mainnet
    fix: before mainnet
    
    release: mainnet
    
    release: v1
    
    fix: mainnet change signer

--- script/Tmp.s.sol
+++ script/Tmp.s.sol
@@ -4,12 +4,45 @@ pragma solidity 0.8.14;
 
 import "forge-std/Script.sol";
 import { ProfileNFT } from "../src/core/ProfileNFT.sol";
+import { CyberEngine } from "../src/core/CyberEngine.sol";
+import { LibDeploy } from "./libraries/LibDeploy.sol";
+import { PermissionedFeeCreationMw } from "../src/middlewares/profile/PermissionedFeeCreationMw.sol";
 
 contract TempScript is Script {
     function run() external {
-        ProfileNFT p = ProfileNFT(0x5A1Bd07533677D389EcAd9C4B1C5D8A3bce99418);
-        // console.log(p.tokenURI(2));
-        // console.log(p.getMetadata(2));
-        console.log(p.getAvatar(2));
+        // vm.startBroadcast();
+        ProfileNFT profile = ProfileNFT(
+            0x5A1Bd07533677D389EcAd9C4B1C5D8A3bce99418
+        );
+        CyberEngine engine = CyberEngine(
+            0xE8805326f9DA84e70c680429eD46B924b3F158F2
+        );
+        PermissionedFeeCreationMw mw = PermissionedFeeCreationMw(
+            0x70FEb3686075b7f99b9D4C1e8238bBEA995292F1
+        );
+        address link3Treasury = address(
+            0xe75Fe33b0fB1441a11C5c1296e5Ca83B72cfE00d
+        );
+        address link3Signer = address(
+            0x2A2EA826102c067ECE82Bc6E2B7cf38D7EbB1B82
+        );
+
+        // engine.setProfileMw(
+        //     address(profile),
+        //     address(mw),
+        //     abi.encode(
+        //         link3Signer,
+        //         link3Treasury,
+        //         LibDeploy._INITIAL_FEE_TIER0,
+        //         LibDeploy._INITIAL_FEE_TIER1,
+        //         LibDeploy._INITIAL_FEE_TIER2,
+        //         LibDeploy._INITIAL_FEE_TIER3,
+        //         LibDeploy._INITIAL_FEE_TIER4,
+        //         LibDeploy._INITIAL_FEE_TIER5
+        //     )
+        // );
+        console.log(mw.getSigner(address(profile)));
+
+        // vm.stopBroadcast();
     }
 }
---
+++ script/animation_url/mainnet-1/SetAnimationURL.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.14;
+
+import "forge-std/Script.sol";
+import { ProfileNFT } from "../../../src/core/ProfileNFT.sol";
+import { Link3ProfileDescriptor } from "../../../src/periphery/Link3ProfileDescriptor.sol";
+import { Create2Deployer } from "../../libraries/Create2Deployer.sol";
+import { LibDeploy } from "../../libraries/LibDeploy.sol";
+import { DeploySetting } from "../../libraries/DeploySetting.sol";
+
+contract SetAnimationURL is Script, DeploySetting {
+    address internal link3Profile = 0x8CC6517e45dB7a0803feF220D9b577326A12033f;
+    string internal animationUrl =
+        "https://cyberconnect.mypinata.cloud/ipfs/bafkreidztiie5tmfvadt52nnb4q2g2whglrnsyhyk7d43hwczh65xjtwni";
+
+    function run() external {
+        _setDeployParams();
+        // make sure only on anvil
+        require(block.chainid == 1, "ONLY_MAINNET");
+        vm.startBroadcast();
+
+        LibDeploy.deployLink3Descriptor(
+            vm,
+            deployParams.deployerContract,
+            true,
+            animationUrl,
+            link3Profile,
+            deployParams.link3Owner
+        );
+
+        vm.stopBroadcast();
+    }
+}
