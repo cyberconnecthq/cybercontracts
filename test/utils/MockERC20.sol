0deecfcdee2be51a88eed9811052166a6fd517cf
Steven Pu <pusongqi@gmail.com>
2022-08-23 13:22:37 -0700

    Feat: Paid Subscribe Middleware + tests

--- test/utils/MockERC20.sol
+++ test/utils/MockERC20.sol
@@ -6,7 +6,7 @@ import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        // Mint 100 tokens to msg.sender
+        // Mint tokens to msg.sender
         // Similar to how
         // 1 dollar = 100 cents
         // 1 token = 1 * (10 ** decimals)
