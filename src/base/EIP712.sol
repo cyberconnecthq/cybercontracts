88ec3cf445a7c00a44c95747fda1ed5e02729092
zhimao <zhimao_liu@icloud.com>
2022-08-25 17:22:37 -0700

    fix: check v,s value

--- src/base/EIP712.sol
+++ src/base/EIP712.sol
@@ -50,6 +50,13 @@ abstract contract EIP712 {
         uint256 deadline
     ) internal view {
         require(deadline >= block.timestamp, "DEADLINE_EXCEEDED");
+        require(
+            uint256(s) <=
+                0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0,
+            "INVALID_SIGNATURE_S_VAULE"
+        );
+        require(uint8(v) == 27 || uint8(v) == 28, "INVALID_SIGNATURE_V_VAULE");
+
         address recoveredAddress = ecrecover(digest, v, r, s);
         require(recoveredAddress == expectedSigner, "INVALID_SIGNATURE");
     }
