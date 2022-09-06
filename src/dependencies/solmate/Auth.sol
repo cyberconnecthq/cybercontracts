c46176c22c859abbaa86a830a0ef376c5a2a5555
zhimao <zhimao_liu@icloud.com>
2022-09-06 15:50:08 -0700

    fix: add error msg

--- src/dependencies/solmate/Auth.sol
+++ src/dependencies/solmate/Auth.sol
@@ -43,7 +43,7 @@ abstract contract Auth is Initializable {
     function setAuthority(Authority newAuthority) public virtual {
         // We check if the caller is the owner first because we want to ensure they can
         // always swap out the authority even if it's reverting or using up a lot of gas.
-        require(msg.sender == owner || authority.canCall(msg.sender, address(this), msg.sig));
+        require(msg.sender == owner || authority.canCall(msg.sender, address(this), msg.sig), "UNAUTHORIZED");
 
         authority = newAuthority;
 
