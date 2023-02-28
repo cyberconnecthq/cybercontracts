c00c2aaad790ab70d53cbbcb7b52f17332bf430e
Zhimao <zhimao_liu@icloud.com>
2023-02-28 10:25:00 +0800

    feat: deploy checker

--- script/DeployChecker.s.sol
+++ script/DeployChecker.s.sol
@@ -18,6 +18,20 @@ contract DeployScript is Script, DeploySetting {
                 address(0x57e12b7a5F38A7F9c23eBD0400e6E53F2a45F271), // link3 namespace
                 true
             );
+        } else if (block.chainid == DeploySetting.BNBT) {
+            LibDeploy.deployRelationshipChecker(
+                vm,
+                LibDeploy.DeployParams(true, true, deployParams),
+                address(0x57e12b7a5F38A7F9c23eBD0400e6E53F2a45F271), // link3 namespace
+                true
+            );
+        } else if (block.chainid == DeploySetting.BNB) {
+            LibDeploy.deployRelationshipChecker(
+                vm,
+                LibDeploy.DeployParams(true, true, deployParams),
+                address(0x2723522702093601e6360CAe665518C4f63e9dA6), // link3 namespace
+                true
+            );
         }
         vm.stopBroadcast();
     }
