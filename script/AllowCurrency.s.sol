0be74a5d786576fbac5ab1b5be3a02d0d414e3ee
Zhimao Liu <zhimao_liu@icloud.com>
2023-03-08 13:51:51 +0800

    deploy: allow erc20 address (#416)

--- script/AllowCurrency.s.sol
+++ script/AllowCurrency.s.sol
@@ -17,6 +17,12 @@ contract DeployScript is Script, DeploySetting {
                 address(0x3963744012daDf90A9034Ea1068f53108B1A3834), // Treasury Address
                 address(0x326C977E6efc84E512bB9C30f76E30c160eD06FB) // Currency Address
             );
+        } else if (block.chainid == DeploySetting.BNB) {
+            LibDeploy.allowCurrency(
+                vm,
+                address(0x90137F1234C137C4284dd317303F2717c871f70A), // Treasury Address
+                address(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56) // Currency Address
+            );
         }
         vm.stopBroadcast();
     }
